012b917efe4340426193c425a47d0a630fd7fada
toraseserara-1 <151169184+toraseserara-1@users.noreply.github.com>
2023-11-29 14:53:03 +0900

    Update 学校.py

--- メインサイト/学校.py
+++ メインサイト/学校.py
@@ -39,3 +39,21 @@ if submit_btn:
     minite_main = minite
     second_main = second1
     content_main = content
+    
+def handle_json(s: str, file_name: str = "sample.json"):
+  import json
+
+  key = "my_key" # 任意のキー
+  dic = {key:s}
+
+  # 保存
+  with open(file_name, "w") as f:
+    json.dump(dic, f, ensure_ascii=False) # ensure_ascii=False を削除すると日本語が文字コードとして出力される
+
+  # 読込
+  with open(file_name, "r") as f:
+    dic = json.load(f)
+    s = dic[key]
+    print(s)
+
+handle_json("Hoge/ふが\n#\tピヨ")
